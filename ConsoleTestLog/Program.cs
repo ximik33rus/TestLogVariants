7a7a841ef3b10e361b689352e18a609d9df73020
golovanov_s <golovanov_s@IT-PC158>
2018-04-10 10:11:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleTestLog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace ConsoleTestLog
+{
+    class Program
+    {
+        public static ILog log = LogManager.GetLogger(string.Empty);
+        public static int a = 0;
+        public static int b = 1;
+        public static int c = 0;
+        static void Main(string[] args)
+        {
+            try
+            {
+                c = b / a; // тут возникнет исключение, так как у нас в массиве только 4 элемента
+                log.Info("Ок");
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+            }
+            finally
+            {
+                log.Info("Блок finally");
+            }
+
+        }
+    }
+}
---
+++ ConsoleTestNLog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace ConsoleTestNLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+                Logger logger = LogManager.GetCurrentClassLogger();
+
+
+                    logger.Trace("trace message");
+
+                    logger.Debug("debug message");
+
+                    logger.Info("info message");
+
+                    logger.Warn("warn message");
+
+                    logger.Error("error message");
+
+                    logger.Fatal("fatal message");
+
+            
+        }
+    }
+}
